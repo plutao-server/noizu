a7719c85e8308ff31fa2f79c676e75179290d56b
shishudesu <shishudesu@gmail.com>
2024-05-24 15:29:02 +0530

    updated test

--- opensimplex_test.go
+++ opensimplex_test.go
@@ -9,14 +9,14 @@ import (
 )
 
 func TestNoise2D(t *testing.T) {
-	var noizu = NewNoise(2342342)
+	var noizu = NewNoise(24234242342)
 	var HEIGHT = 1024
 	var WIDTH = 1024
 	img := image.NewAlpha(image.Rect(0, 0, WIDTH, HEIGHT))
 	for i := 0; i <= WIDTH; i++ {
 		for j := 0; j <= HEIGHT; j++ {
 
-			var noise = noizu.Noise2D(float64(i)/float64(WIDTH), float64(j)/float64(HEIGHT))
+			var noise = noizu.Noise2D(float64(i), float64(j))
 			img.SetAlpha(i, j, color.Alpha{A: uint8(noise * 127.0)})
 
 		}
